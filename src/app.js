8e388e7e2d0391b3a37d8d3255ea0d7505ec1507
Ben Grabowski <gbowski30@gmail.com>
2019-09-24 10:15:59 -0600

    added logger from winston

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston')
 const { NODE_ENV } = require('./config')
 const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
@@ -11,6 +12,20 @@ const app = express()
 const morganOption = (NODE_ENV === 'production') 
 ? 'tiny'
 : 'common';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: 'info.log' })
+    ]
+  });
+  
+  if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  }
 
 app.use(morgan(morganOption))
 app.use(helmet())
