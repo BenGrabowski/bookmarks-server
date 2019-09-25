1d361441185cbe73f77412f4f0a6685158b4c6a9
Ben Grabowski <gbowski30@gmail.com>
2019-09-25 11:58:21 -0600

    syntax error in bookmarks.sql

--- src/bookmarks.sql
+++ src/bookmarks.sql
@@ -10,13 +10,13 @@ create table bookmarks (
 
 insert into bookmarks (title, url, rating, description)
 values
-    ('Facebook', 'www.facebook.com', 3),
-    ('Amazon', 'www.amazon.com', 5),
+    ('Facebook', 'www.facebook.com', 3, NULL),
+    ('Amazon', 'www.amazon.com', 5, NULL,
     ('ESPN', 'www.ESPN.com', 4, 'Sports News'),
-    ('Twitter', 'www.twitter.com', 5),
+    ('Twitter', 'www.twitter.com', 5, NULL),
     ('Bloc', 'www.bloc.thinkful.com', 4),
     ('Netflix', 'www.netflix.com', 4, 'Video streaming'),
-    ('HBO', 'www.hbogo.com', 5),
-    ('Soundcloud', 'www.soundcloud.com', 2),
+    ('HBO', 'www.hbogo.com', 5, NULL),
+    ('Soundcloud', 'www.soundcloud.com', 2, 'Music Streaming'),
     ('GMail', 'www.mail.google.com', 5, 'Email service'),
-    ('Google', 'www.google.com', 3, 'Search Engine')
+    ('Google', 'www.google.com', 3, 'Search Engine');
